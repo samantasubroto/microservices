569ac58a09fba554dc57247579794d71bc2183ac
Subrotho Samantha <ssubrotho@kaartech.com>
2024-04-30 18:07:52 +0530

    Second commit

--- QuizServices/QuizServices/src/main/java/com/quiz/services/QuestionClient.java
+++ QuizServices/QuizServices/src/main/java/com/quiz/services/QuestionClient.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-
-@FeignClient(url = "http://localhost:8079",value="Question-Client")
+//(url = "http://localhost:8079",value="Question-Client")
+@FeignClient(name = "QUESTIONSERVICES")
 public interface QuestionClient {
 
     @GetMapping("/question/quiz/{quizId}")
